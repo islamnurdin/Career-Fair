8625a94e12e6d2829343dfef50251c3a78b76de1
aisa.tashmatova <aisa.tashmatova@gmail.com>
2018-09-04 17:57:54 +0600

    fff

--- Career Fair/AppDelegate.swift
+++ Career Fair/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,8 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        IQKeyboardManager.shared.enable = true
         
         return true
     }
--- Career Fair/Extensions.swift
+++ Career Fair/Extensions.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 extension UIView {
-    func applyStyleButton() {
+    func applyStyleView() {
         self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
         self.layer.shadowOffset = CGSize(width: 0.0, height: 7.0)
+        self.layer.backgroundColor = UIColor.black.cgColor
         self.layer.shadowOpacity = 1
         self.layer.shadowRadius = 7
     }
@@ -28,5 +29,14 @@ extension UIView {
         border.backgroundColor = color.cgColor
         border.frame = CGRect(x: self.frame.width - 1, y: 0, width: width, height: self.frame.size.height)
         self.layer.addSublayer(border)
+    }
+}
+
+extension UITableViewCell {
+    func applyStyleViewCell() {
+        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 7.0)
+        self.layer.shadowOpacity = 1
+        self.layer.shadowRadius = 7
     }
 }
