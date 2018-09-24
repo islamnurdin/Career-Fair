043f77fcd233826e87ca3ec4c2b198cf43eba6da
mamadnazar <shohkarim7@gmail.com>
2018-09-24 17:49:51 +0600

    minor pollitions

--- Career Fair/Menu Files/Organizators/OrganizatorsViewController.swift
+++ Career Fair/Menu Files/Organizators/OrganizatorsViewController.swift
@@ -11,71 +11,10 @@ import Alamofire
 
 class OrganizatorsViewController: UIViewController{
 
-    @IBOutlet weak var detailLabel: UILabel!
-    @IBOutlet weak var collVIew: UICollectionView!
-    
-    var organizer = [Organizer]()
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Организаторы"
-        
-        getOrganizerData {
-            self.collVIew.reloadData()
-        }
-
-    }
-    
-    func getOrganizerData(completed: @escaping () -> ()) {
-        let url = URL(string: "http://138.68.86.126/organizer")
-        Alamofire.request(url!).responseJSON { response in
-            let data = response.data
-            do {
-                self.organizer = try JSONDecoder().decode([Organizer].self, from: data!)
-                DispatchQueue.main.async {
-                    completed()
-                }
-            }
-            catch let e{
-                print(e)
-            }
-        }
-    }
-}
-
-extension OrganizatorsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return organizer.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "organizatorsCell", for: indexPath) as! OrgCollectionViewCell
-    
-        cell.name.text = organizer[indexPath.row].full_name
-        let urlString = organizer[indexPath.row].logo_url
-        let url = URL(string: urlString)
-        
-        cell.image.downloadedFrom(url: url!)
-        cell.image.applyStylesToImage()
-        return cell
-    }
-
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        detailLabel.text = organizer[indexPath.row].description
-        
-        if let cell = self.collVIew.cellForItem(at: indexPath) as? OrgCollectionViewCell {
-            UICollectionViewCell.animate(withDuration: 1, animations: {
-                cell.image.reloadView()
-            })
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        if let cell = self.collVIew.cellForItem(at: indexPath) as? OrgCollectionViewCell {
-            UICollectionViewCell.animate(withDuration: 1, animations: {
-                cell.image.reloadView()
-            })
-        }
     }
 }
